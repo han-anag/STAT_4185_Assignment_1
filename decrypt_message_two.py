21d8ed7bba3200bafb9c6626d4c672d412d632f7
han-anag <114708039+han-anag@users.noreply.github.com>
2023-09-19 01:00:05 -0400

    decrypted message two

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,20 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+message = list(encrypted_message)
+
+a = 0
+b = len(message)-1
+
+while a < b:
+    c = message[b]
+    message[b] = message[a]
+    message[a] = c
+    a += 2
+    b -= 2
+
+message.reverse()
+
+message_str = ''.join(message)
+print(message_str)
+
