25d4408294628307fc722da49beba684f288a4fd
han-anag <114708039+han-anag@users.noreply.github.com>
2023-09-19 00:18:57 -0400

    added hw files

---
+++ decrypt_message_one.py
@@ -0,0 +1,54 @@
+cipher = {
+    'a':'v',
+    'b':'h',
+    'c':'r',
+    'd':'j',
+    'e':'t',
+    'f':'x',
+    'g':'s',
+    'h':'a',
+    'i':'e',
+    'j':'f',
+    'k':'b',
+    'l':'n',
+    'm':'o',
+    'n':'i',
+    'o':'g',
+    'p':'l',
+    'q':'m',
+    'r':'z',
+    's':'q',
+    't':'u',
+    'u':'c',
+    'v':'k',
+    'w':'d',
+    'x':'p',
+    'y':'y',
+    'z':'w',
+    ' ': '}',
+    '\n': '^',
+    ',': '5',
+    '!': '[',
+    ':':'-',
+    ')':'*',
+    '.': '%' 
+}
+
+encrypted_file = open("encrypted_message_one.txt", 'r')
+
+encrypted_message = encrypted_file.readline()
+
+encrypted_file.close()
+
+# Write code below
+msg = []
+reverse_cipher = {cipher[i]:i for i in cipher}
+
+# print(reverse_cipher)
+
+for letter in encrypted_message:
+    #I know this is not very efficient at all but I'm too lazy to output it as a list
+    msg = msg + reverse_cipher[letter]
+
+print(encrypted_message)
+print(msg)
